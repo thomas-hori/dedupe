3054044beee1e90a2d8ecbd96b21063c83331c23
Thomas Hori <thomas.hori@liddicott.com>
2013-08-17 19:59:46 +0100

    In light of face-to-face communication, avoid recursive symlinks with dumped hard copy, and be more modular (level of modularity: class).
    Signed-off-by: Thomas Hori <thomas.hori@liddicott.com>

--- dedupe.py
+++ dedupe.py
@@ -2,42 +2,66 @@
 #Prints matches on Windows so far
 
 import os,time
-def megalink(a,b):
-    os.unlink(b)
-    os.symlink(a,b)
-if os.name=="nt":
-    #Otherwise dangerous: IT WOULD BE UNLINKED BUT NOT SYMLINKED!!! HAHAHAHAHAAAAAAAA
-    def megalink(a,b):
-        print "I would link",`a`,"to",`b`,"but I can't."
-try:
-    from hashlib import md5 #For newer pythons
-except ImportError:
-    from md5 import md5 #For older pythons
-
 import shelve #No deletes so even dumbdbm will kinda work well
-revhashes=shelve.open("hashes_temp.db","n")#{} #Dict fills memory like no tomorrow
-for root,dirs,files in os.walk(os.getcwd()):
-    if (".git" in root) or (".hg" in root) or (".bzr" in root) or (".rcs" in root):
-        #Skip VCSs
-        continue
-    for f in files:
-        p=os.path.join(root,f)
-        fd=open(p,"rb")
-        b=fd.read()
-        fd.close()
-        hash=md5(b).digest() #Not hexdigest as it takes up 2x the space and there is no need for being ascii
-        if hash in revhashes:
-            for uvver in revhashes[hash]:
+
+class Hasher(object):
+    #Should be a mutable object implementing the mapping protocol: hash to sequence
+    revhashes=None 
+    def strip_data(self,buffer):
+        """Remove metadata from the buffer and return it, for (e.g.) MP3 data comparison.
+        A hook for mixins."""
+        return buffer
+    def check_file(self,path,buffer):
+        """Check for duplicates of a file, return path if successful, None otherwise"""
+        raise NotImplementedError("should be implemented by subclass")
+    def megalink(self,a,b):
+        """Link a to b, possibly bringing metadata into harmony on the way (according to subclass)"""
+        if os.name=="nt":
+            #DO NOT even approach the unlinking stage on Windows!
+            print "I would link",`a`,"to",`b`,"but I can't."
+            return
+        os.unlink(b)
+        try:
+            os.link(a,b)
+        except: #XXX except what?  IOError?  OSError?  RuntimeError?  GrueError?  YASDError?
+            os.symlink(a,b)
+class ExactMd5Hasher(Hasher):
+    def __init__(self):
+        self.revhashes=shelve.open("md5hasher.db","n")#{} #Dict fills memory like no tomorrow
+    def check_file(self,p,b):
+        try:
+            import hashlib
+        except ImportError:
+            import md5 as hashlib
+        b=self.strip_data(b)
+        hash=hashlib.md5(b).digest() #Not hexdigest as it takes up 2x the space and there is no need for being ascii
+        if hash in self.revhashes:
+            for uvver in self.revhashes[hash]:
                 fd2=open(uvver,"rb")
                 b2=fd2.read()
                 fd2.close()
                 if b2==b:
-                    megalink(uvver,p)
-                    break
-            else:
-                revhashes[hash]+=(p,) #Tuples as collision rare - thus optimised
+                    self.megalink(uvver,p)
+                    return
+            self.revhashes[hash]+=(p,) #Tuples as collision rare - thus optimised
         else:
-            revhashes[hash]=(p,) #Tuples as collision rare - thus optimised
+            self.revhashes[hash]=(p,) #Tuples as collision rare - thus optimised
+        return None
+hashers=[ExactMd5Hasher()]
+for root,dirs,files in os.walk(os.getcwd()):
+    #XXX check if these repodirs are corect
+    if (".git" in root) or (".hg" in root) or (".bzr" in root) or (".rcs" in root):
+        continue
+    for f in files:
+        p=os.path.join(root,f)
+        if os.path.islink(p):
+            #Skip symlinks
+            continue
+        fd=open(p,"rb")
+        b=fd.read()
+        fd.close()
+        for hasher in hashers:
+            hasher.check_file(p,b)
         time.sleep(0.1) #Let the OS breath!
 
 #unlinking the db is complicated as any one (or pair) of the following may be present:
